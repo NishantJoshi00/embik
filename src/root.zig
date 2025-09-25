0594ba8190bb847af5e29a41345f09b19a203d27
Nishant Joshi <nishantjo.12@gmail.com>
2025-09-25 00:33:28 -0700

    chore: fine tune tests

--- src/root.zig
+++ src/root.zig
@@ -88,8 +88,8 @@ test "embed perf large text" {
     const end = std.time.milliTimestamp();
     const duration = end - start;
 
-    // expected operation cost <10ms
-    try std.testing.expect(@divFloor(duration, iterations) < 10);
+    // expected operation cost <8ms
+    try std.testing.expect(@divFloor(duration, iterations) < 8);
 }
 
 test "embed perf extra large text" {
@@ -98,7 +98,7 @@ test "embed perf extra large text" {
     var engine = try embed.EmbeddingEngine.init(model_path);
     defer engine.deinit();
 
-    const text = "In a village of La Mancha, the name of which I have no desire to call to mind, there lived not long since one of those gentlemen that keep a lance in the lance-rack, an old buckler, a lean hack, and a greyhound for coursing. An olla of rather more beef than mutton, a salad on most nights, some lean chorizo, and a pigeon or so constituted his whole diet. He was thin and gaunt, with a face that was weathered and wrinkled from years of outdoor life. His eyes were sharp and bright, reflecting a keen intellect and a restless spirit. He had a habit of reading books of chivalry and adventure, which he devoured with great enthusiasm. This habit had given him a somewhat romantic view of the world, and he often dreamed of embarking on his own adventures. One day, after finishing yet another book on knights and their exploits, he decided to take action. He donned an old suit of armor that had belonged to his great-grandfather, mounted his trusty steed Rocinante, and set out to seek his fortune as a knight-errant. His first challenge came when he encountered a group of windmills on the horizon. Mistaking them for giants, he charged at them with all his might, only to be knocked off his horse by the force of the windmill's blades. Undeterred by this setback, he continued on his journey, determined to prove his valor and win glory in the eyes of the world.";
+    const text = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.";
     const iterations = 10;
 
     const start = std.time.milliTimestamp();
@@ -110,6 +110,6 @@ test "embed perf extra large text" {
     const end = std.time.milliTimestamp();
     const duration = end - start;
 
-    // expected operation cost <40ms
-    try std.testing.expect(@divFloor(duration, iterations) < 40);
+    // expected operation cost <13ms
+    try std.testing.expect(@divFloor(duration, iterations) < 13);
 }
